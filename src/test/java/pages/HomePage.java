6d92c792a6adce67026614b2b0b0c4df99780e2e
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-12-12 15:17:38 +0300

    Added HomePage

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class HomePage extends BasePage {
+    public HomePage(AppiumDriver<MobileElement> driver) {
+        super(driver);
+        initElements(this);
+    }
+
+    @Override
+    public HomePage isPageOpened() {
+        return this;
+    }
+}
