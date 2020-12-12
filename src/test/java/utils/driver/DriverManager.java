e04e1c99b9cbf9ae87f8464b66df139df1b82e0c
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-12-12 15:17:16 +0300

    Added DriverManager

---
+++ src/test/java/utils/driver/DriverManager.java
@@ -0,0 +1,28 @@
+package utils.driver;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+public abstract class DriverManager {
+
+    AppiumDriver driver;
+    DesiredCapabilities cap = new DesiredCapabilities();
+    
+    protected abstract AppiumDriver createDriver(AppiumDriverLocalService service, String udid, String deviceName);
+
+    public AppiumDriver getDriver(AppiumDriverLocalService service, String deviceName, String platform, String udid) {
+        if (null == driver) {
+            cap.setCapability(MobileCapabilityType.PLATFORM_NAME, platform);
+            cap.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
+            cap.setCapability("udid", udid);
+            driver = createDriver(service, udid, deviceName);
+        }
+        return driver;
+    }
+}
+
+    
+   
