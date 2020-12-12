d823ce47db594888aaadd18872540f74df02c759
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-12-12 16:47:03 +0300

    Added locators for iOS

--- src/test/java/pages/SignInPage.java
+++ src/test/java/pages/SignInPage.java
@@ -2,8 +2,21 @@ package pages;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 
 public class SignInPage extends BasePage {
+    @iOSXCUITFindBy(accessibility = "username-field")
+    @AndroidFindBy(id = "com.instagram.android:id/log_in_button")
+    public MobileElement userNameInput;
+
+    @iOSXCUITFindBy(accessibility = "password-field")
+    @AndroidFindBy(id = "com.instagram.android:id/log_in_button")
+    public MobileElement passwordInput;
+
+    @iOSXCUITFindBy(accessibility = "login-button")
+    @AndroidFindBy(id = "com.instagram.android:id/log_in_button")
+    public MobileElement logInButton;
 
     public SignInPage(AppiumDriver<MobileElement> driver) {
         super(driver);
@@ -16,14 +29,19 @@ public class SignInPage extends BasePage {
     }
 
     public SignInPage typeEmail(String email) {
+        userNameInput.clear();
+        userNameInput.sendKeys(email);
         return this;
     }
 
     public SignInPage typePassword(String password) {
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
         return this;
     }
 
-    public HomePage clickNextButton() {
+    public HomePage clickLogInButton() {
+        logInButton.click();
         return new HomePage(driver);
     }
 }
