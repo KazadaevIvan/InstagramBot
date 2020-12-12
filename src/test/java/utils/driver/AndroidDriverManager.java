9bd0847e607cc932518f9e55d72605ced9830e4a
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-12-12 15:15:40 +0300

    Added AndroidDriverManager

---
+++ src/test/java/utils/driver/AndroidDriverManager.java
@@ -0,0 +1,28 @@
+package utils.driver;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import utils.PropertyManager;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class AndroidDriverManager extends DriverManager {
+
+    @Override
+    public AppiumDriver createDriver(AppiumDriverLocalService service, String udid, String deviceName) {
+        cap.setCapability("appActivity",PropertyManager.getInstance().get("application.activity.name"));
+        cap.setCapability("appPackage", PropertyManager.getInstance().get("application.package.name"));
+        cap.setCapability("app", new File(PropertyManager.getInstance().get("application.path")).getAbsolutePath());
+        cap.setCapability("udid", udid);
+        if(udid.contains("emulator")){
+            cap.setCapability("avd", deviceName);
+            cap.setCapability("avdLaunchTimeout", 300000);
+            cap.setCapability("avdReadyTimeout", 300000);
+        }
+        driver = new AndroidDriver(service, cap);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        return driver;
+    }
+}
