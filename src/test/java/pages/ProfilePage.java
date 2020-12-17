16800733d42273e63fa22cd4f7468adf0d6ac1a1
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-12-17 16:55:38 +0300

    Added isPrivate and hasPhotos methods

--- src/test/java/pages/ProfilePage.java
+++ src/test/java/pages/ProfilePage.java
@@ -10,7 +10,7 @@ public class ProfilePage extends BasePage {
     public static final String FOLLOWERS_LOCATOR_IOS = "user-detail-header-followers";
     public static final String FOLLOWERS_LOCATOR_ANDROID = "com.instagram.android:id/row_profile_header_followers_container";
     public static final String FOLLOW_BUTTON_LOCATOR_IOS = "user-detail-header-follow-button";
-    public static final String FOLLOW_BUTTON_LOCATOR_ANDROID = "//android.widget.Button[@text='Follow']";
+    public static final String FOLLOW_BUTTON_LOCATOR_ANDROID = "(//android.widget.Button[@text='Follow'])[1]";
     public static final String LIST_VIEW_LOCATOR_IOS = "//XCUIElementTypeCollectionView[@name='profile']";
     public static final String LIST_VIEW_LOCATOR_ANDROID = "com.instagram.android:id/layout_container_main";
     public static final String BACK_BUTTON_LOCATOR_IOS = "//XCUIElementTypeNavigationBar/XCUIElementTypeButton[1]";
@@ -18,13 +18,16 @@ public class ProfilePage extends BasePage {
     public static final String PHOTO_LIST_LOCATOR_IOS = "//XCUIElementTypeButton[@name='media-thumbnail-cell']";
     public static final String PHOTO_LIST_LOCATOR_ANDROID = "//androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout/android.widget.ImageView";
     public static final String PRIVATE_ACCOUNT_TITLE_LOCATOR_IOS = "This account is private";
-    public static final String PRIVATE_ACCOUNT_TITLE_LOCATOR_ANDROID = "com.instagram.android:id/empty_state_view_title";
+    public static final String PRIVATE_ACCOUNT_TITLE_LOCATOR_ANDROID = "com.instagram.android:id/row_profile_header_empty_profile_notice_container";
+    public static final String POSTS_NUMBER_LOCATOR_IOS = "user-detail-header-media-button";
+    public static final String POSTS_NUMBER_LOCATOR_ANDROID = "com.instagram.android:id/row_profile_header_textview_post_count";
     public MobileElement followers;
     public MobileElement followButton;
     public MobileElement listView;
     public MobileElement backButton;
     public List<MobileElement> photoList;
-    public MobileElement privateAccountTitle;
+    public List<MobileElement> privateAccountTitle;
+    public MobileElement postsNumber;
 
     public ProfilePage(AppiumDriver<MobileElement> driver) {
         super(driver);
@@ -37,6 +40,7 @@ public class ProfilePage extends BasePage {
                 break;
             case ("Android"):
                 followers = driver.findElementById(FOLLOWERS_LOCATOR_ANDROID);
+                break;
         }
         return followers;
     }
@@ -48,6 +52,7 @@ public class ProfilePage extends BasePage {
                 break;
             case ("Android"):
                 listView = driver.findElementById(LIST_VIEW_LOCATOR_ANDROID);
+                break;
         }
         return listView;
     }
@@ -59,6 +64,7 @@ public class ProfilePage extends BasePage {
                 break;
             case ("Android"):
                 photoList = driver.findElementsByXPath(PHOTO_LIST_LOCATOR_ANDROID);
+                break;
         }
         return photoList;
     }
@@ -81,6 +87,7 @@ public class ProfilePage extends BasePage {
                 break;
             case ("Android"):
                 followButton = driver.findElementByXPath(FOLLOW_BUTTON_LOCATOR_ANDROID);
+                break;
         }
         followButton.click();
         return this;
@@ -103,8 +110,34 @@ public class ProfilePage extends BasePage {
                 break;
             case ("Android"):
                 backButton = driver.findElementById(BACK_BUTTON_LOCATOR_ANDROID);
+                break;
         }
         backButton.click();
         return new ProfileFollowsPage(driver);
+    }
+
+    public Boolean isPrivate() {
+        switch (platform) {
+            case ("iOS"):
+                privateAccountTitle = driver.findElementsByAccessibilityId(PRIVATE_ACCOUNT_TITLE_LOCATOR_IOS);
+                break;
+            case ("Android"):
+                privateAccountTitle = driver.findElementsById(PRIVATE_ACCOUNT_TITLE_LOCATOR_ANDROID);
+                break;
+        }
+        return privateAccountTitle.size() > 0;
+    }
+
+    public Boolean hasPhotos() {
+        switch (platform) {
+            case ("iOS"):
+                postsNumber = driver.findElementByAccessibilityId(POSTS_NUMBER_LOCATOR_IOS);
+                return Integer.parseInt(postsNumber.getText().substring(0, postsNumber.getText().indexOf(" "))) > 0;
+            case ("Android"):
+                postsNumber = driver.findElementById(POSTS_NUMBER_LOCATOR_ANDROID);
+                return Integer.parseInt(postsNumber.getText()) > 0;
+            default: return null;
+        }
+
     }
 }
