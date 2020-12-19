81385b336420dcb0578fec4094696feaca32e7c4
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-12-19 13:49:47 +0300

    Updated PRIVATE_ACCOUNT_TITLE_LOCATOR_ANDROID locator

--- src/test/java/pages/ProfilePage.java
+++ src/test/java/pages/ProfilePage.java
@@ -19,7 +19,7 @@ public class ProfilePage extends BasePage {
     public static final String PHOTO_LIST_LOCATOR_IOS = "//XCUIElementTypeButton[@name='media-thumbnail-cell']";
     public static final String PHOTO_LIST_LOCATOR_ANDROID = "//androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout/android.widget.ImageView";
     public static final String PRIVATE_ACCOUNT_TITLE_LOCATOR_IOS = "This account is private";
-    public static final String PRIVATE_ACCOUNT_TITLE_LOCATOR_ANDROID = "com.instagram.android:id/row_profile_header_empty_profile_notice_container";
+    public static final String PRIVATE_ACCOUNT_TITLE_LOCATOR_ANDROID = "//*[@text='This Account is Private']";
     public static final String POSTS_NUMBER_LOCATOR_IOS = "user-detail-header-media-button";
     public static final String POSTS_NUMBER_LOCATOR_ANDROID = "com.instagram.android:id/row_profile_header_textview_post_count";
     public MobileElement followers;
@@ -129,7 +129,7 @@ public class ProfilePage extends BasePage {
                 privateAccountTitle = driver.findElementsByAccessibilityId(PRIVATE_ACCOUNT_TITLE_LOCATOR_IOS);
                 break;
             case ("Android"):
-                privateAccountTitle = driver.findElementsById(PRIVATE_ACCOUNT_TITLE_LOCATOR_ANDROID);
+                privateAccountTitle = driver.findElementsByXPath(PRIVATE_ACCOUNT_TITLE_LOCATOR_ANDROID);
                 break;
         }
         return privateAccountTitle.size() > 0;
