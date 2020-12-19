db82c8e86a313c6081701ab6493f04647bf7e3e6
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-12-19 13:33:38 +0300

    Added FluentWait

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -2,6 +2,10 @@ package pages;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import java.time.Duration;
 
 abstract public class BasePage {
     public static final String SEARCH_PAGE_LOCATOR_IOS = "explore-tab";
@@ -28,6 +32,11 @@ abstract public class BasePage {
                 searchPageIcon = driver.findElementByAccessibilityId(SEARCH_PAGE_LOCATOR_ANDROID);
                 break;
         }
+        new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(21))
+                .pollingEvery(Duration.ofSeconds(3))
+                .ignoring(StaleElementReferenceException.class)
+                .until(driver -> searchPageIcon.isDisplayed());
         searchPageIcon.click();
         return new SearchPage(driver);
     }
