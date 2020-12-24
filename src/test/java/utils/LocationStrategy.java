73bae0c3725723e3fbf7a4ec5c4b98f0680797e2
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-12-24 17:12:29 +0300

    Added static variables for storing content of json file with locators

--- src/test/java/utils/LocationStrategy.java
+++ src/test/java/utils/LocationStrategy.java
@@ -13,6 +13,18 @@ public class LocationStrategy {
     AppiumDriver<MobileElement> driver;
     String platform;
 
+    public static String fileContentIOS;
+    public static String fileContentAndroid;
+
+    static {
+        try {
+            fileContentIOS = fileReader("src/test/resources/iOSLocators.json");
+            fileContentAndroid = fileReader("src/test/resources/androidLocators.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public LocationStrategy(AppiumDriver<MobileElement> driver) {
         this.driver = driver;
         platform = (String) driver.getCapabilities().getCapability("platformName");
@@ -28,7 +40,7 @@ public class LocationStrategy {
         return builder.toString();
     }
 
-    public MobileElement getElement(String name) throws IOException {
+    public MobileElement getElement(String name) {
         switch (getElementTypeFromFile(getFileContent(), name).get(0)) {
             case ("xpath"):
                 return driver.findElementByXPath(getElementValueFromFile(getFileContent(), name).get(0));
@@ -41,7 +53,7 @@ public class LocationStrategy {
         }
     }
 
-    public List<MobileElement> getElementsList(String name) throws IOException {
+    public List<MobileElement> getElementsList(String name) {
         switch (getElementTypeFromFile(getFileContent(), name).get(0)) {
             case ("xpathList"):
                 return driver.findElementsByXPath(getElementValueFromFile(getFileContent(), name).get(0));
@@ -54,20 +66,20 @@ public class LocationStrategy {
         }
     }
 
-    public MobileElement getElement(String name, String parameter) throws IOException {
+    public MobileElement getElement(String name, String parameter) {
         return driver.findElementByXPath(String.format(getElementValueFromFile(getFileContent(), name).get(0), parameter));
     }
 
-    public List<MobileElement> getElementsList(String name, String parameter) throws IOException {
+    public List<MobileElement> getElementsList(String name, String parameter) {
         return driver.findElementsByXPath(String.format(getElementValueFromFile(getFileContent(), name).get(0), parameter));
     }
 
-    public String getFileContent() throws IOException {
+    public String getFileContent() {
         switch (platform) {
             case ("iOS"):
-                return fileReader("src/test/resources/iOSLocators.json");
+                return fileContentIOS;
             case ("Android"):
-                return fileReader("src/test/resources/androidLocators.json");
+                return fileContentAndroid;
             default:
                 return null;
         }
