6e12d270c6f497807fb7d59f8e8c19d2e2d99d76
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-12-24 19:53:53 +0300

    Renamed class, added steps and method's descriptions

---
+++ src/test/java/tests/FollowUsersTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class FollowUsersTest extends BaseTest {
+    @BeforeMethod(description = "Login")
+    public void login() throws IOException {
+        loginSteps
+                .login(email, password);
+    }
+
+    @Test(description = "Get some followers of profile and follow them")
+    public void usersShouldBeFollowed() throws IOException {
+        homePage
+                .openSearchPage();
+        searchPageSteps
+                .findProfile(profile);
+        for (String profile : profilePageSteps
+                .getFollowers(numberOfProfilesToFollow)) {
+            profileFollowsPage
+                    .openSearchPage();
+            searchPageSteps
+                    .findProfile(profile);
+            profilePageSteps
+                    .attemptToSetLikeToTheFirstPhoto()
+                    .followProfile();
+        }
+    }
+}
