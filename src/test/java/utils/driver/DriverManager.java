3f0d15fd1a5411cdb5b413118f807c041e95a1da
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-12-27 16:34:00 +0300

    Updated getDriver method

--- src/test/java/utils/driver/DriverManager.java
+++ src/test/java/utils/driver/DriverManager.java
@@ -1,6 +1,7 @@
 package utils.driver;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -8,12 +9,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public abstract class DriverManager {
 
-    AppiumDriver driver;
+    AppiumDriver<MobileElement> driver;
     DesiredCapabilities cap = new DesiredCapabilities();
 
-    protected abstract AppiumDriver createDriver(AppiumDriverLocalService service, String udid, String deviceName);
+    protected abstract AppiumDriver<MobileElement> createDriver(AppiumDriverLocalService service, String udid, String deviceName);
 
-    public AppiumDriver getDriver(AppiumDriverLocalService service, String deviceName, String platform, String udid) {
+    public AppiumDriver<MobileElement> getDriver(AppiumDriverLocalService service, String deviceName, String platform, String udid) {
         if (null == driver) {
             cap.setCapability(MobileCapabilityType.PLATFORM_NAME, platform);
             cap.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
